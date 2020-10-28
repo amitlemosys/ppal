259d032583d73d97d2bb94397b06012072b482cc
amitlemosys <68587477+amitlemosys@users.noreply.github.com>
2020-10-29 02:21:08 +0530

    'setup'

---
+++ resources/views/form.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+@section('content')
+  <div >
+    <div >
+      <div >
+        @if(session()->has('message'))
+          <p >
+            {{ session('message') }}
+          </p>
+        @endif
+        <div >
+          <div >
+            <img src="{{ asset('images/paypal.png') }}" >
+          </div>
+          <div >
+            <img src="{{ asset('images/laravel.png') }}" >
+          </div>
+        </div>
+        <p><strong>Order Overview !</strong></p>
+        <hr>
+        <p>Item : Yearly Subscription cost !</p>
+        <p>Amount : ${{ $service->amount }}</p>
+        <hr>
+      </div>
+      <div >
+        @php $transaction_id=rand(100,1000); @endphp
+        <form method="POST" action="{{ route('checkout.payment.paypal', [$service->id,'transaction_id' => $transaction_id]) }}">
+          {{ csrf_field() }}
+          <button >
+            <i aria-hidden="true"></i> Pay with PayPal
+          </button>
+        </form>
+      </div>
+    </div>
+  </div>
+@stop
